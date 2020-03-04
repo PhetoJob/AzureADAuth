ab9f1f79a003e01472b4747c83022c8793723be6
Job, van der Toom <jobto@youforce.net>
2020-03-04 11:56:20 +0100

    Client secret retrieved from keyvault

--- AzureADAuth/Consumer/Controllers/ConsumerController.cs
+++ AzureADAuth/Consumer/Controllers/ConsumerController.cs
@@ -7,6 +7,8 @@ using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Identity.Client;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
@@ -17,6 +19,12 @@ namespace Consumer.Controllers
     [ApiController]
     public class ConsumerController : ControllerBase
     {
+        private string keyVaultUrl;
+
+        private string tenantId;
+
+        private string clientId;
+
         private string serviceUrl;
 
         private string resource;
@@ -25,12 +33,11 @@ namespace Consumer.Controllers
 
         public ConsumerController(IConfiguration configuration)
         {
+            keyVaultUrl = configuration["KeyVault:Url"];
+            tenantId = configuration["AzureAD:TenantId"];
+            clientId = configuration["AzureAD:ClientId"];
             serviceUrl = configuration["Service:BaseUrl"] + "/api/data";
             resource = configuration["Service:AzureAD:AppIdScope"];
-            app = ConfidentialClientApplicationBuilder.Create(configuration["AzureAD:ClientId"])
-                .WithAuthority(AzureCloudInstance.AzurePublic, configuration["AzureAD:TenantId"])
-                .WithClientSecret(configuration["AzureAD:ClientSecret"])
-                .Build();
         }
 
         [HttpGet]
@@ -49,9 +56,28 @@ namespace Consumer.Controllers
 
         private async Task<string> GetTokenForService()
         {
-            var scopes = new string[] { resource };
-            var result = await app.AcquireTokenForClient(scopes).ExecuteAsync();
-            return result.AccessToken;
+            try
+            {
+                if (app == null)
+                {
+                    var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                    var keyVault = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+                    var secret = await keyVault.GetSecretAsync(keyVaultUrl, "consumerSecret");
+
+                    app = ConfidentialClientApplicationBuilder.Create(clientId)
+                        .WithAuthority(AzureCloudInstance.AzurePublic, tenantId)
+                        .WithClientSecret(secret.Value)
+                        .Build();
+                }
+
+                var scopes = new string[] { resource };
+                var result = await app.AcquireTokenForClient(scopes).ExecuteAsync();
+                return result.AccessToken;
+            }
+            catch(Exception e)
+            {
+                return "An error occured while trying to acquire token: " + e;
+            }
         }
 
         private async Task<string> GetDataFromService(string token)
